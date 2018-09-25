a46c40897a0af4f67968db5672dd5734c2e1a334
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2018-09-25 19:27:09 +0900

    add finchers/v0.12.0-alpha.7

---
+++ docs/finchers/v0.12.0-alpha.7/implementors/core/ops/deref/trait.Deref.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["finchers"] = [{text:"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"finchers/input/query/struct.Serde.html\" title=\"struct finchers::input::query::Serde\">Serde</a>&lt;T&gt;",synthetic:false,types:["finchers::input::query::Serde"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"finchers/input/struct.Input.html\" title=\"struct finchers::input::Input\">Input</a>",synthetic:false,types:["finchers::input::Input"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
