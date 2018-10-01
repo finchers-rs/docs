641934039b995514af4ee590003dce12d1f95964
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2018-10-01 22:04:05 +0900

    add finchers-session/v0.1.0

---
+++ docs/finchers-session/v0.1.0/implementors/core/clone/trait.Clone.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["finchers_session"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"finchers_session/cookie/struct.CookieBackend.html\" title=\"struct finchers_session::cookie::CookieBackend\">CookieBackend</a>",synthetic:false,types:["finchers_session::cookie::CookieBackend"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"finchers_session/in_memory/struct.InMemoryBackend.html\" title=\"struct finchers_session::in_memory::InMemoryBackend\">InMemoryBackend</a>",synthetic:false,types:["finchers_session::in_memory::InMemoryBackend"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"finchers_session/redis/struct.RedisBackend.html\" title=\"struct finchers_session::redis::RedisBackend\">RedisBackend</a>",synthetic:false,types:["finchers_session::redis::RedisBackend"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
